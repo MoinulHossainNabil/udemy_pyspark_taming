d99a9f34a6f08c19a443e5abd39e552a3449c1ac
MoinulHossainNabil <44189517+MoinulHossainNabil@users.noreply.github.com>
2019-07-31 08:17:56 +0600

    Update Assignmetn_Amount_spent_by_customer.py

--- src/Assignmetn_Amount_spent_by_customer.py
+++ src/Assignmetn_Amount_spent_by_customer.py
@@ -9,12 +9,12 @@ def customerDataSet(file):
     lines = file.split(",")
     customerId = int(lines[0])
     productPrice = float(lines[2])
-    return (customerId,productPrice)
+    return (customerId, productPrice)
 
 input = sc.textFile("/home/bjit-542/Big Data/SparkCourse/customer-orders.csv")
 customerIdPrice = input.map(customerDataSet)
-result = customerIdPrice.reduceByKey(lambda x,y: x+y)
-sortedBySpentAmount = result.map(lambda x: (x[1],x[0])).sortByKey().collect()
+result = customerIdPrice.reduceByKey(lambda x, y: x+y)
+sortedBySpentAmount = result.map(lambda x: (x[1], x[0])).sortByKey().collect()
 
 for s in sortedBySpentAmount:
     print(s)
