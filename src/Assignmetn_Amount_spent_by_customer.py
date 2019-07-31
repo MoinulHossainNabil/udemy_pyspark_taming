be66350d58580d2e766de078a3a14d516a39e419
MoinulHossainNabil <44189517+MoinulHossainNabil@users.noreply.github.com>
2019-07-31 08:15:15 +0600

    Convention Managed

--- src/Assignmetn_Amount_spent_by_customer.py
+++ src/Assignmetn_Amount_spent_by_customer.py
@@ -1,19 +1,21 @@
 
 # Here is my code snippet for finding total amount of spent by each unique customerId
-from pyspark import SparkConf,SparkContext
-conf=SparkConf().setMaster("local").setAppName("CustomerOrder")
-sc=SparkContext.getOrCreate()
-def customerDataSet(file):
-    lines=file.split(",")
-    customerId=int(lines[0])
-    productPrice=float(lines[2])
-    return (customerId,productPrice)
-input = sc.textFile("/home/bjit-542/Big Data/SparkCourse/customer-orders.csv")
-customerIdPrice=input.map(customerDataSet)
+from pyspark import SparkConf, SparkContext
 
-#customerIdPrice.foreach(lambda c: print(c))
-result=customerIdPrice.reduceByKey(lambda x,y: x+y)
-sortedBySpentAmount=result.map(lambda x: (x[1],x[0])).sortByKey().collect()
+conf = SparkConf().setMaster("local").setAppName("CustomerOrder")
+sc = SparkContext.getOrCreate()
+
+def customerDataSet(file):
+    lines = file.split(",")
+    customerId = int(lines[0])
+    productPrice = float(lines[2])
+    return (customerId,productPrice)
+
+input = sc.textFile("/home/bjit-542/Big Data/SparkCourse/customer-orders.csv")
+customerIdPrice = input.map(customerDataSet)
+result = customerIdPrice.reduceByKey(lambda x,y: x+y)
+sortedBySpentAmount = result.map(lambda x: (x[1],x[0])).sortByKey().collect()
+
 for s in sortedBySpentAmount:
     print(s)
 
